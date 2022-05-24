8e6ac3522dec4a4dd2d7f876c76e943f3b038663
StaceyA132 <101208038+StaceyA132@users.noreply.github.com>
2022-05-24 12:09:34 -0400

    Create MammalTest.java

---
+++ MammalTest.java
@@ -0,0 +1,10 @@
+public class MammalTest {
+	public static void main(String []args) {
+		
+		Mammal mammal1 = new Mammal();// creating object type mammal  
+		
+		System.out.println(mammal1.getNumOfLegs());
+	
+
+	}
+}
